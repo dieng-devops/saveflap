df39deccfcca91eb351fffcade5a734fbc0b8b7a
Nicolas Rodriguez <nicoladmin@free.fr>
2017-11-25 01:23:17 +0100

    Load application config from env vars

---
+++ config/initializers/01-figaro.rb
@@ -0,0 +1,24 @@
+# Check that all environment variables are present before running the application.
+
+APPLICATION_CONFIG = {
+  rails_config: %w(
+    SECRET_KEY_BASE
+  ),
+
+  db_config: %w(
+    DB_ADAPTER
+    DB_HOST
+    DB_PORT
+    DB_NAME
+    DB_USER
+    DB_PASS
+  ),
+
+}.freeze
+
+begin
+  Figaro.require_keys(*APPLICATION_CONFIG.values.flatten)
+rescue Figaro::MissingKeys => e
+  puts "\n#{e.message}"
+  exit 1
+end
