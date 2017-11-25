c7b85e7d9cbac940a7903e1e245762fed5bd8769
Nicolas Rodriguez <nicoladmin@free.fr>
2017-11-25 04:45:27 +0100

    Add link menu to go to Admin page

--- config/navigation/user_navigation.rb
+++ config/navigation/user_navigation.rb
@@ -2,6 +2,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |menu|
     menu.dom_id = 'side-menu'
     menu.dom_class = 'nav'
-    menu.item :homepage,  { icon: icon_for_entry('fa-home'), text: label_for_entry(t('text.homepage')) }, root_path, highlights_on: /\A\/\z/
+    menu.item :homepage,  { icon: icon_for_entry('fa-home'),  text: label_for_entry(t('text.homepage')) }, root_path, highlights_on: /\A\/\z/
+    menu.item :admin,     { icon: icon_for_entry('fa-gears'), text: label_for_entry(t('text.administration')) }, admin_root_path
   end
 end
