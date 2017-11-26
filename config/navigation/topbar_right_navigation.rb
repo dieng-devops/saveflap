917afc22b4b436b2fa787a53658624c0625a9cf3
Nicolas Rodriguez <nicoladmin@free.fr>
2017-11-26 03:38:07 +0100

    Add logout link

--- config/navigation/topbar_right_navigation.rb
+++ config/navigation/topbar_right_navigation.rb
@@ -1,5 +1,9 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |menu|
     menu.dom_class = 'navbar-top-links navbar-right'
+    menu.item :logged, icon('user'), '#', highlights_on: %r(/my), class: 'dropdown' do |sub_menu|
+      sub_menu.auto_highlight = false
+      sub_menu.item :logout, { icon: icon_for_entry('fa-sign-out'), text: label_for_entry(t('text.logout')) }, destroy_user_session_path, method: :delete
+    end
   end
 end
