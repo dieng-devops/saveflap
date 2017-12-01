51e60bbf4b1e3b3f0c90fd2712eeb52a7217b809
Nicolas Rodriguez <nicoladmin@free.fr>
2017-12-01 03:18:17 +0100

    Fix form attributes label translation

--- lib/flap/core_ext/action_form_patch.rb
+++ lib/flap/core_ext/action_form_patch.rb
@@ -1,3 +1,5 @@
+# Define our own Result object to respect Trailblazer expectations
+# See: https://github.com/trailblazer/reform/blob/master/lib/reform/form/call.rb
 module Contract
   class Result < SimpleDelegator
     def initialize(success, data)
@@ -17,6 +19,9 @@ end
 
 module Flap
   module CoreExt
+
+    # Add '#call' wrapper method to ActionForm::Base
+    # A '#call' method is required by Trailblazer::Operation as a convention
     module ActionFormPatch
       def call(params = {})
         submit(params)
@@ -24,7 +29,28 @@ module Flap
         ::Contract::Result.new(bool, self)
       end
     end
+
+    # Override Trailblazer::Rails::Controller#run to bypass the use of Trailblazer::Rails:Form object
+    # (useless and break form attributes translations)
+    # See: https://github.com/trailblazer/trailblazer-rails/blob/master/lib/trailblazer/rails/controller.rb
+    module TrailblazerPatch
+      def run(operation, params=self.params, *dependencies)
+        result = operation.(
+          _run_params(params),
+          *_run_runtime_options(*dependencies)
+        )
+
+        @form  = result["contract.default"]
+        @model = result["model"]
+
+        yield(result) if result.success? && block_given?
+
+        @_result = result
+      end
+    end
+
   end
 end
 
 ActionForm::Base.prepend(Flap::CoreExt::ActionFormPatch)
+Trailblazer::Rails::Controller.prepend(Flap::CoreExt::TrailblazerPatch)
