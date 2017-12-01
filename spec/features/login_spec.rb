f95773811f43cf27b879f5a9ef6687c40eec8412
Nicolas Rodriguez <nicoladmin@free.fr>
2017-12-01 22:45:18 +0100

    Use example.net as domain form mail address

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 feature 'Login', js: true do
   scenario 'When existing user try to login' do
-    FactoryBot.create(:user, email: "toto@jbox-web.com", password: 'blabla', password_confirmation: 'blabla')
+    FactoryBot.create(:user, email: "toto@example.net", password: 'blabla', password_confirmation: 'blabla')
     load_server_host
     visit main_app.root_path
-    fill_in :user_email, with: 'toto@jbox-web.com'
+    fill_in :user_email, with: 'toto@example.net'
     fill_in :user_password, with: 'blabla'
     find("[type=submit]").click
     expect(page).to have_content('Accueil')
   end
 
   scenario 'When disabled user try to login' do
-    FactoryBot.create(:user, email: "toto@jbox-web.com", password: 'blabla', password_confirmation: 'blabla', enabled: false)
+    FactoryBot.create(:user, email: "toto@example.net", password: 'blabla', password_confirmation: 'blabla', enabled: false)
     load_server_host
     visit main_app.root_path
-    fill_in :user_email, with: 'toto@jbox-web.com'
+    fill_in :user_email, with: 'toto@example.net'
     fill_in :user_password, with: 'blabla'
     find("[type=submit]").click
     expect(page).to have_content('Compte inactif')
