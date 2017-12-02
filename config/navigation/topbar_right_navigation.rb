80c31dfea9c9aaa95fcd8d665b8b8e942a9b516e
Nicolas Rodriguez <nicoladmin@free.fr>
2017-12-02 22:47:10 +0100

    Display current user email in top right

--- config/navigation/topbar_right_navigation.rb
+++ config/navigation/topbar_right_navigation.rb
@@ -1,7 +1,7 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |menu|
     menu.dom_class = 'navbar-top-links navbar-right'
-    menu.item :logged, icon('user'), '#', highlights_on: %r(/my), class: 'dropdown' do |sub_menu|
+    menu.item :logged, current_user.email, '#', highlights_on: %r(/my), class: 'dropdown' do |sub_menu|
       sub_menu.auto_highlight = false
       sub_menu.item :my_account, { icon: icon_for_entry('fa-user'),     text: label_for_entry(t('text.my_account')) }, my_account_path, highlights_on: %r(/my)
       sub_menu.item :divider,    *menu_divider
