7e5cfbca210b254fff9855eaf5c4e1a31955b416
Nicolas Rodriguez <nicoladmin@free.fr>
2017-12-03 01:27:23 +0100

    Add IconsHelper module

---
+++ app/helpers/common/icons_helper.rb
@@ -0,0 +1,19 @@
+module Common
+  module IconsHelper
+
+    def bool_to_icon(bool, opts = {})
+      bool ? check_icon(opts) : cross_icon(opts)
+    end
+
+
+    def check_icon(opts = {})
+      icon 'check', { class: 'fa-success' }.merge(opts)
+    end
+
+
+    def cross_icon(opts = {})
+      icon 'times', { class: 'fa-important' }.merge(opts)
+    end
+
+  end
+end
