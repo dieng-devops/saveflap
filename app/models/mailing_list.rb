fb792b227f2c22a429ca5a8bbde7b590f3194d10
Nicolas Rodriguez <nicoladmin@free.fr>
2017-12-14 18:03:40 +0100

    When deleting MailingList, delete associated Emails too

--- app/models/mailing_list.rb
+++ app/models/mailing_list.rb
@@ -1,7 +1,7 @@
 class MailingList < ApplicationRecord
 
   # Relations
-  has_many :emails, inverse_of: :mailing_list
+  has_many :emails, inverse_of: :mailing_list, dependent: :destroy
 
   # Validations
   validates :name, uniqueness: true
