a4525e27c6b8f5266567e9c8522bccf07ee218ad
Nicolas Rodriguez <nicoladmin@free.fr>
2018-02-15 11:59:43 +0100

    Disable Sidekiq check (not use for now)

--- config/initializers/health_monitor.rb
+++ config/initializers/health_monitor.rb
@@ -7,5 +7,5 @@ HealthMonitor.configure do |config|
   config.redis.configure do |redis_config|
     redis_config.url = "redis://#{Settings.redis_host}:#{Settings.redis_port}/#{REDIS_DB_MAP.index('health_check')}"
   end
-  config.add_custom_provider(Flap::Monitoring::Sidekiq)
+  # config.add_custom_provider(Flap::Monitoring::Sidekiq)
 end
